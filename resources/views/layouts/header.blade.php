68a55b2a41e702601500bbbb9305a28c34726efb
stefanadisurya <stefanadisurya@gmail.com>
2021-02-10 10:22:01 +0700

    add master.blade.php

---
+++ resources/views/layouts/header.blade.php
@@ -0,0 +1,115 @@
+<div class="collapse navbar-collapse d-inline-block" id="navbarNav">
+    <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+    </button>
+    @guest
+      {{-- Jika guest yang mengakses halaman --}}
+      <ul class="navbar-nav mr-auto">
+        <li class="nav-item">
+            <a class="nav-link text-white js-scroll-trigger mr-3" href=" {{ route('home') }}">Home</a>
+        </li>
+
+        <li class="nav-item dropdown">
+            <a id="navbarDropdown" class="nav-link dropdown-toggle text-white mr-3" href="#" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false" v-pre>
+              Categories
+            </a>
+
+            <div class="dropdown-menu dropdown-menu-right" aria-labelledby="navbarDropdown">
+                <a class="dropdown-item text-dark" href="{{ route('home') }}">
+                  Beach
+                </a>
+
+                <a class="dropdown-item text-dark" href="{{ route('home') }}">
+                    Mountain
+                </a>
+              </div>
+        </li>
+
+        <li class="nav-item">
+            <a class="nav-link text-white js-scroll-trigger mr-3" href=" {{ route('home') }}">About Us</a>
+        </li>
+      </ul>
+    @else
+        @if(auth()->user()->role=="Admin")
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link text-white js-scroll-trigger mr-3" href=" {{ route('home') }}">Home</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link text-white js-scroll-trigger mr-3" href=" {{ route('home') }}">Admin</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link text-white js-scroll-trigger mr-3" href=" {{ route('home') }}">User</a>
+                </li>
+            </ul>
+
+        @elseif(auth()->user()->role=="User")
+            <ul class="navbar-nav mr-auto">
+                <li class="nav-item">
+                    <a class="nav-link text-white js-scroll-trigger mr-3" href=" {{ route('home') }}">Home</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link text-white js-scroll-trigger mr-3" href=" {{ route('home') }}">Profile</a>
+                </li>
+
+                <li class="nav-item">
+                    <a class="nav-link text-white js-scroll-trigger mr-3" href=" {{ route('home') }}">Blog</a>
+                </li>
+            </ul>
+      @endif
+    @endguest
+  
+      <ul class="navbar-nav ml-auto">
+        @guest
+            {{-- Jika guest yang mengakses halaman --}}
+            @if (Route::has('register'))
+                <li class="nav-item">
+                    <a class="nav-link text-white js-scroll-trigger mr-3" href="{{ route('register') }}">
+                        <i class="fas fa-user"></i>
+                        {{ __('Sign Up') }}
+                    </a>
+                </li>      
+            @endif
+
+            <li class="nav-item">
+              <a class="nav-link text-white js-scroll-trigger mr-3" href="{{ route('login') }}">
+                <i class="fas fa-sign-out-alt mr-2"></i>
+                {{ __('Login') }}
+            </a>
+            </li>
+            
+        @else
+            {{-- Jika admin yang mengakses halaman --}}
+            @if(auth()->user()->role=="Admin")
+                <li class="nav-item">
+                    <a class="nav-link text-white js-scroll-trigger mr-3" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </li>
+  
+            {{-- Jika guest yang mengakses halaman --}}
+            @elseif(auth()->user()->role=="User")
+                <li class="nav-item">
+                    <a class="nav-link text-white js-scroll-trigger mr-3" href="{{ route('logout') }}"
+                    onclick="event.preventDefault();
+                                document.getElementById('logout-form').submit();">
+                    {{ __('Logout') }}
+                    </a>
+
+                    <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                        @csrf
+                    </form>
+                </li>
+            @endif
+        @endguest
+      </ul>
+  </div>
---
+++ resources/views/layouts/master.blade.php
@@ -0,0 +1,65 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+    
+    {{-- Font Awesome CDN --}}
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css">
+
+    <title>
+      @yield('title')
+    </title>
+
+    {{-- <script src="{{ asset('js/app.js') }}" defer></script> --}}
+
+    <!-- Fonts -->
+    <link rel="dns-prefetch" href="//fonts.gstatic.com">
+    <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
+
+    <!-- Styles -->
+    <link href="{{ asset('css/app.css') }}" rel="stylesheet">
+
+    @yield('styles')
+  
+    <style>
+      @media (max-width: 768px) {
+        .navbar-nav .nav-item:not(:last-child) {
+          border-right: none;
+        }
+      }
+    </style>
+
+    <link href="https://fonts.googleapis.com/css?family=Poppins:100,100i,200,200i,300,300i,400,400i,500,500i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
+
+
+  </head>
+  <body>
+    <nav class="navbar navbar-expand-lg navbar-light bg-dark" id="mainNav">
+      <div class="container">
+          @include('layouts.header')
+      </div>
+  </nav>
+
+  @yield('content')
+
+    <!-- Option 1: jQuery and Bootstrap Bundle (includes Popper) -->
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+
+    <!-- Option 2: jQuery, Popper.js, and Bootstrap JS
+    <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.min.js" integrity="sha384-w1Q4orYjBQndcko6MimVbzY0tgp4pWB4lZ7lr30WKz0vr/aWKhXdBNmNb5D92v7s" crossorigin="anonymous"></script>
+    -->
+
+    {{-- @include('sweetalert::alert') --}}
+  </body>
+</html>
